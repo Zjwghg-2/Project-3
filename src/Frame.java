6e4e6ffb4841c9c6a0c9baa4110e21850ad25c90
Zjwghg-2 <matthew.beecher@student.nmt.edu>
2022-11-18 19:20:08 -0700

    New Frame Format: [SourceNetwork][SourceNode][DestNetwork][DestNode][SequenceNumber][CRC][Size][Ack/Data] Added separate ack frame and data frame constructors Adjusted / added methods as necessary Added documentation

--- src/Frame.java
+++ src/Frame.java
@@ -3,35 +3,100 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.util.Arrays;
 
+/**
+ * Frame object class
+ * Format: [SourceNetwork][SourceNode][DestNetwork][DestNode][SequenceNumber][CRC][Size][Ack/Data]
+ */
 public class Frame {
-    private final int source, dest, size;
+    private final int sourceNode, sourceNet, destNode, destNet, ack, size, sequence;
+    byte crc;
     private final String data;
 
     /**
-     * Frame class constructor
-     * @param source source port
-     * @param dest destination port
+     * Frame class constructor (data)
+     * @param sourceNet source network (-1 if unknown, 0 if control)
+     * @param sourceNode source node ID
+     * @param destNet destination network (0 if control)
+     * @param destNode destination node ID
+     * @param sequence frame sequence number
      * @param data Data value (send "" for ack)
      */
-    public Frame(int source, int dest, String data){
-        this.source = source;
-        this.dest = dest;
+    public Frame(int sourceNet, int sourceNode, int destNet, int destNode, int sequence, String data){
+        this.sourceNode = sourceNode;
+        this.sourceNet = sourceNet;
+        this.destNode = destNode;
+        this.destNet = destNet;
+        this.sequence = sequence;
         this.size = data.length();
         this.data = data;
+        this.crc = this.calcCrc();
+        //ack is not sent, so this is just a placeholder value since ack is defined as an object field
+        this.ack = -1;
+    }
+    /**
+     * Frame class constructor (ack)
+     * @param sourceNet source network (-1 if unknown, 0 if control)
+     * @param sourceNode source node ID
+     * @param destNet destination network (0 if control)
+     * @param destNode destination node ID
+     * @param sequence sequence number
+     * @param ack Ack value (unique implementation)
+     */
+    public Frame(int sourceNet, int sourceNode, int destNet, int destNode, int sequence, int ack){
+        this.sourceNode = sourceNode;
+        this.sourceNet = sourceNet;
+        this.destNode = destNode;
+        this.destNet = destNet;
+        this.sequence = sequence;
+        this.size = 0;
+        this.ack = ack;
+        this.crc = this.calcCrc();
+        //data is not sent, so this is just a placeholder value since ack is defined as an object field
+        this.data = "";
     }
 
-    public int getSource(){
-        return this.source;
+
+    /*-------------Methods--------------*/
+
+
+    /**
+     * Get source from frame
+     * @return size 2 integer array; 0 is network ID, 1 is node ID
+     */
+    public int[] getSource(){
+        return new int[]{this.sourceNet, this.sourceNode};
     }
-    public int getDest(){
-        return this.dest;
+    /**
+     * Get dest from frame
+     * @return size 2 integer array; 0 is network ID, 1 is node ID
+     */
+    public int[] getDest(){
+        return new int[]{this.destNet, this.destNode};
     }
-    public int getSize(){
-        return this.size;
+
+    //other getters
+    public int getSize(){return this.size;}
+    public int getAck(){return this.ack;}
+    public String getData(){return this.data;}
+    public byte getCrc(){return this.crc;}
+    public int setSN(){return this.sequence;}
+
+    //calculate expected crc byte value for a given frame
+    public byte calcCrc(){
+        byte a = 0;
+        a+= this.sourceNet + this.sourceNode + this.destNet + this.destNode + this.sequence;
+        if(this.size > 0){
+            //if data frame use data
+            a+=this.size + data.getBytes()[0];
+        } else {
+            //if ack frame use ack
+            a+=this.ack;
+        }
+        return a;
     }
-    public String getData(){
-        return this.data;
-    }
+
+    //private helper function
+    private void setCrc(byte crc){this.crc = crc;}
 
     /**
      * Encode frame into byte array message
@@ -39,11 +104,29 @@ public class Frame {
      * @return frame message
      */
     public byte[] encode(){
-        byte[] out = new byte[3+this.size];
-        out[0] = (byte) this.source;
-        out[1] = (byte) this.dest;
-        out[2] = (byte) this.size;
-        if (this.size > 0) System.arraycopy(data.getBytes(), 0, out, 3, this.size);
+        byte[] out;
+        if(this.size > 0){
+            //data frame
+            out = new byte[7+this.size];
+        } else {
+            //ack frame
+            out = new byte[8];
+        }
+        //set standard values
+        out[0] = (byte) this.sourceNet;
+        out[1] = (byte) this.sourceNode;
+        out[2] = (byte) this.destNet;
+        out[3] = (byte) this.destNode;
+        out[4] = (byte) this.sequence;
+        out[5] = this.crc;
+        out[6] = (byte) this.size;
+        if (this.size > 0){
+            //set data if data frame
+            System.arraycopy(data.getBytes(), 0, out, 7, this.size);
+        } else {
+            //set ack if ack frame
+            out[7] = (byte) ack;
+        }
         return out;
     }
 
@@ -53,13 +136,31 @@ public class Frame {
      * @return new frame object
      */
     public static Frame decode(byte[] msg) throws FrameLostException{
-        //read in standard 3 values
-        int source = Byte.toUnsignedInt(msg[0]), dest = Byte.toUnsignedInt(msg[1]), size = Byte.toUnsignedInt(msg[2]);
-        //check for data loss
-        if(msg.length != 3 + size) throw new FrameLostException("Frame decoding detected data loss");
-        //parse data and return
-        String data = new String(Arrays.copyOfRange(msg, 3, 3+size));
-        return new Frame(source, dest, data);
+        Frame f;
+        //read in standard 5 values
+        int sourceNet = Byte.toUnsignedInt(msg[0]), sourceNode = Byte.toUnsignedInt(msg[1]),
+                destNet = Byte.toUnsignedInt(msg[2]), destNode = Byte.toUnsignedInt(msg[3]),
+                sequence = Byte.toUnsignedInt(msg[4]), size = Byte.toUnsignedInt(msg[6]);
+        byte crc = msg[5];
+        //data frame
+        if(size > 0){
+            //check for loss
+            if(msg.length != 7 + size) throw new FrameLostException("Frame decoding detected data loss");
+            //parse data and frame it
+            String data = new String(Arrays.copyOfRange(msg, 6, 6+size));
+            f = new Frame(sourceNet, sourceNode, destNet, destNode, sequence, data);
+        }
+        //ack frame
+        else{
+            //check for loss
+            if(msg.length != 8) throw new FrameLostException("Frame decoding detected data loss");
+            //parse data and frame it
+            int ack = Byte.toUnsignedInt(msg[7]);
+            f = new Frame(sourceNet, sourceNode, destNet, destNode, sequence, ack);
+        }
+        //set crc (ensure original crc tag is used in case of data failure)
+        f.setCrc(crc);
+        return f;
     }
 
     /**
@@ -69,33 +170,61 @@ public class Frame {
      * @return new frame object
      */
     public static Frame decodeFromChannel(DataInputStream in) throws FrameLostException, IOException {
-        int source = 0, dest = 0, size = 0;
+        int sourceNet, sourceNode, destNet, destNode, size, sequence;
+        byte crc;
+        Frame f;
         //read standard parameters
         try {
-            //read 1 frame of data
-            source = in.read();
-            dest = in.read();
+            //read header
+            sourceNet = in.read();
+            sourceNode = in.read();
+            destNet = in.read();
+            destNode = in.read();
+            sequence = in.read();
+            crc = in.readByte();
             size = in.read();
         } catch (EOFException e){
             //EOF was encountered unexpectedly here, so there has been data loss.
             throw new FrameLostException("Frame decoding detected data loss");
         }
-        //Check for ack frame
-        if (size == 0) return new Frame(source, dest, "");
-        //Not ack frame, get data
-        byte[] data = in.readNBytes(size);
-        //Verify data size and return
-        if(data.length != size) throw new FrameLostException("Frame decoding detected data loss");
-        return new Frame(source, dest, new String(data));
+        //ack frame
+        if (size == 0){
+            int ack;
+            try{
+                ack = in.read();
+            } catch (EOFException e){
+                //EOF was encountered unexpectedly here, so there has been data loss.
+                throw new FrameLostException("Frame decoding detected data loss");
+            }
+            f = new Frame(sourceNet, sourceNode, destNet, destNode, sequence, ack);
+        }
+        //data frame
+        else {
+            byte[] data = in.readNBytes(size);
+            //Verify data size and return
+            if(data.length != size) throw new FrameLostException("Frame decoding detected data loss");
+            f = new Frame(sourceNet, sourceNode, destNet, destNode, sequence, new String(data));
+        }
+        //set cyc (ensure original crc is used in case of error)
+        f.setCrc(crc);
+        return f;
     }
 
+    @Override
     public String toString(){
-        return "[" + source + "][" + dest + "][" + size + "][" + data + "]";
+        if(this.size >= 0){
+            return "[" + sourceNet + ":" + sourceNode + "][" + destNet + ":" + destNode  + "][" + sequence + "][" +
+                    "][" + crc + "][" + size + "][" + data + "]";
+        }else{
+            return "[" + sourceNet + ":" + sourceNode + "][" + destNet + ":" + destNode  + "][" + sequence + "][" +
+                    "][" + crc + "][" + size + "][" + ack + "]";
+        }
     }
+
 
     //A test example to show how to utilize this class, and to show that it works.
     public static void main(String[] args) throws Exception{
-        Frame f1 = new Frame(0, 1, "woah there its a message lol lmao");
+        Frame f1 = new Frame(0, 0, 0, 0, 0, "woah there its a message lol lmao");
         byte[] e1 = f1.encode();
 
         Frame f2 = Frame.decode(e1);
